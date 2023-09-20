e1ee5dcb43d4bfb8d75eb543aea72f3762302eaf
Safarova1 <samasafarova1998@gmail.com>
2023-09-20 14:03:03 +0400

    praktika 3

--- JavaScript cw/lesson_2 if- else- else if/script.js
+++ JavaScript cw/lesson_2 if- else- else if/script.js
@@ -1,26 +1,54 @@
-let cins = prompt('cinsivizi daxil edin- ')
-let yas = prompt('yasinizi daxil edin- ')
-yas = Number(yas)
+// 100 qeder ne daxil etsek cavi qaytarsin on bir
+
+let cavab
+let input = prompt()
+
+birinciReqem = input[0]
+ikinciReqem = input[1]
+
+if (input > 10 ){
+    switch(birinciReqem){
+        case  '1' :
+            alert('on')
+        switch(ikinciReqem){
+            case '2':
+                alert('iki')
+        }
+    }    
+}
 
 
-let cavab = cins === 'kisi' && yas > 18 ? 'kisi qeydiyyata alindi': 
-            cins === 'qadin' && yas > 17 ? 'qadin qeydiyyata alindi' : 
-            cins === 'kisi' && yas < 18 ? 'kisi qeydiyyata alinmadi!': 
-            cins === 'qadin' && yas < 17 ? 'qadin qeydiyyata alinmadi!' : 
-            'wrong'
-            
-            
-
-console.log(cavab)
 
 
+// let bir = 1
+// let iki = 2
+// let uc = 'uc'
+// let dort = 'dord'
+// let bes = 'bes'
+// let alti = 'alti'
+// let yeddi = 'yeddi'
+// let sekkiz = 'sekkiz'
+// let doqquz = 'doqquz'
+// let on = 'on'
+// let iyirmi = 'iyirmi'
+// let otuz = 'otuz'
+// let qirx = 'qirx'
+// let elli = 'elli'
+// let altmis = 'altmis'
+// let yetmis ='yetmis'
+// let seksen = 'seksen'
+// let doqsan = 'doqsan'
+// let yuz ='yuz'
 
+// if (input < 10 ){
+//     let cavab = input == bir ? 'bir':
+//                 input == iki ? 'iki': 'wrong'
+//         }
 
-
-
-
-
-
+// else{
+    
+//     let cavab = birinciReqem === 1
+// }
 
 
 
