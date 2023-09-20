57767e8cd40d4abcb282f5500ef1569308e8b26c
Safarova1 <samasafarova1998@gmail.com>
2023-09-20 11:45:39 +0400

    lesson 2

---
+++ JavaScript cw/lesson_2/script.js
@@ -0,0 +1,32 @@
+let input = prompt()
+let inputSecond = prompt()
+
+if (input === 'Baki' || input === 'baki'|| input  === 'BAKI'){ //ve ya
+    alert('Baki')
+}
+else if (input === 'Moskva' && inputSecond === 'Russia') {  //ve  -- 1cini yazib enter-i bas
+    alert(input + '2')
+}
+else if (input === 'Dubai'){
+    alert(input)
+}
+else{
+    alert("Wrong")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
