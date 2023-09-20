f6d956d7f6c592257a7d8bef7f102405cc7e063f
Safarova1 <samasafarova1998@gmail.com>
2023-09-20 11:12:59 +0400

    cevrilmeleri elave etdim

--- JavaScript hw/hw_1_JavaScript giriş/script.js
+++ JavaScript hw/hw_1_JavaScript giriş/script.js
@@ -2,13 +2,13 @@ let bookTitle
 let pageCount 
 let e_book 
 
-bookTitle = "Qürur və Qərəz"
-pageCount = 166
-e_book = true
+bookTitle = String ("Qürur və Qərəz")
+pageCount = Number(166)
+e_book = Boolean(1)
 console.log(`Mənim oxuduğum kitabın adı ${bookTitle}-dir, onun ${pageCount} səhifəsi var. Bu elektron kitabdır: ${e_book}`)
 
 
-bookTitle = "Leyla ilə Məcnun"
-pageCount = 500
-e_book = false
+bookTitle = String("Leyla ilə Məcnun")
+pageCount = Number(500)
+e_book = Boolean(0)
 console.log(`Mənim oxuduğum kitabın adı ${bookTitle}-dur, onun ${pageCount} səhifəsi var. Bu elektron kitabdır: ${e_book}`)
