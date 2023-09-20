055192613db07c85f08dadd388e88f22ecbb817d
Safarova1 <samasafarova1998@gmail.com>
2023-09-20 11:19:54 +0400

    cevrilme add

--- JavaScript hw/hw_1_JavaScript giriş/script.js
+++ JavaScript hw/hw_1_JavaScript giriş/script.js
@@ -1,14 +1,14 @@
 let bookTitle 
 let pageCount 
-let e_book 
+let eBook 
 
 bookTitle = String ("Qürur və Qərəz") 
 pageCount = Number(166)
-e_book = Boolean(1)
+eBook = Boolean(1)
 console.log(`Mənim oxuduğum kitabın adı ${bookTitle}-dir, onun ${pageCount} səhifəsi var. Bu elektron kitabdır: ${e_book}`)
 
 
 bookTitle = String("Leyla ilə Məcnun")
 pageCount = Number(500)
-e_book = Boolean(0)
+eBook = Boolean(0)
 console.log(`Mənim oxuduğum kitabın adı ${bookTitle}-dur, onun ${pageCount} səhifəsi var. Bu elektron kitabdır: ${e_book}`)
