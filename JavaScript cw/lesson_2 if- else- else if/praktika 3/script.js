c1dcf29c050da15094b2daac3870f4363dec66a3
Safarova1 <samasafarova1998@gmail.com>
2023-09-20 13:27:02 +0400

    lesson 2

---
+++ JavaScript cw/lesson_2 if- else- else if/praktika 3/script.js
@@ -0,0 +1,56 @@
+// 1-12 reqemler yaziriq reqeme uygun ay verir
+
+let input = prompt()
+let number = Number(input)
+
+switch(number){
+    case 1:
+        alert('yanvar')
+        break
+    case 2:
+        alert('fevral')
+        break
+
+    case 3:
+        alert('mart')
+        break
+
+    case 4:
+        alert('aprel')
+        break
+
+    case 5:
+        alert('may')
+        break
+    case 6:
+        alert('iyun')
+        break
+    
+    case 7:
+        alert('iyul')
+        break
+    
+    case 8:
+        alert('avqust')
+        break
+
+    case 9:
+        alert('sentyabr')
+        break
+    case 10:
+        alert('oktyabr')
+        break
+        
+    case 11:
+        alert('noyabr')
+        break
+        
+    case 12:
+        alert('dekabr')
+        break
+
+    default:
+        alert('say sehvdir')
+
+
+}
--- JavaScript cw/lesson_2 if- else- else if/script.js
+++ JavaScript cw/lesson_2 if- else- else if/script.js
@@ -1,9 +1,31 @@
-let active = confirm()
+let cins = prompt('cinsivizi daxil edin- ')
+let yas = prompt('yasinizi daxil edin- ')
+yas = Number(yas)
 
-if (!active) {
-    alert('cancel')
-}
 
-else {
-    alert('ok')
-}
+let cavab = cins === 'kisi' && yas > 18 ? 'kisi qeydiyyata alindi': 
+            cins === 'qadin' && yas > 17 ? 'qadin qeydiyyata alindi' : 
+            cins === 'kisi' && yas < 18 ? 'kisi qeydiyyata alinmadi!': 
+            cins === 'qadin' && yas < 17 ? 'qadin qeydiyyata alinmadi!' : 
+            'wrong'
+            
+            
+
+console.log(cavab)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
