7884159df9f549999c9e43ec552bd69e2880acb6
Safarova1 <samasafarova1998@gmail.com>
2023-09-26 20:34:08 +0400

    hw 6

--- JavaScript hw/hw_6_Dovrler/script.js
+++ JavaScript hw/hw_6_Dovrler/script.js
@@ -1,25 +1,56 @@
 // tap1
 
-let text = prompt()
-c = 0
+// let text = prompt()
+// c = 0
 
-for (let i = 0; i < text.length; i++) {
-    if (text[i] === '1' ||
-        text[i] === '2' ||
-        text[i] === '3' ||
-        text[i] === '4' ||
-        text[i] === '5' ||
-        text[i] === '6' ||
-        text[i] === '7' ||
-        text[i] === '8' ||
-        text[i] === '9' ||
-        text[i] === '0') {
-        c++ 
-    }
-}
-if (c!=0){
-    console.log("reqem var textin icinde",c, "dene")
-}
-else{
-    console.log("reqem yoxdur textin icinde")
-}
+// for (let i = 0; i < text.length; i++) {
+//     if (text[i] === '1' ||
+//         text[i] === '2' ||
+//         text[i] === '3' ||
+//         text[i] === '4' ||
+//         text[i] === '5' ||
+//         text[i] === '6' ||
+//         text[i] === '7' ||
+//         text[i] === '8' ||
+//         text[i] === '9' ||
+//         text[i] === '0') {
+//         c++ 
+//     }
+// }
+// if (c!=0){
+//     console.log("reqem var textin icinde",c, "dene")
+// }
+// else{
+//     console.log("reqem yoxdur textin icinde")
+// }
+
+
+
+// tap2
+
+// let text = prompt()
+
+// let ilk ="yoxdur"
+// for (let i = 0; i < text.length; i++) {
+//     if (text[i] === '.' ) {
+//         ilk = i
+//         break
+//     }
+// }
+
+// console.log("ilk defe noqte ", ilk , "indeksinde idi ")
+
+
+
+
+
+
+// tap3
+
+// let sahil = 1
+// for (let i = 1; i <= 10; i++) {
+//     sahil *= i
+// }
+
+// console.log(sahil)
+
