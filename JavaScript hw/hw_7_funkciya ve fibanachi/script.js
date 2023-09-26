178ff87952613a234075101442d1b3049993c77c
Safarova1 <samasafarova1998@gmail.com>
2023-09-26 22:20:35 +0400

    hw 7

---
+++ JavaScript hw/hw_7_funkciya ve fibanachi/script.js
@@ -0,0 +1,44 @@
+// tap1
+
+// let a = 34
+// let b = 5
+// let c = 3
+
+// const newFunction = () => {
+
+
+//     if (a > b && a > c) {
+//         console.log(a)
+//     } else if (b > a && b > c) {
+//         console.log(b)
+//     }else{
+//         console.log(c)
+//     }
+// }
+
+
+// newFunction()
+
+
+
+
+
+//-----------------------------------------
+// tap2
+// const newFunction = () => {
+//     let numOne = 0
+//     console.log(numOne)
+//     let numSecond = 1
+//     console.log(numSecond)
+
+//     let sum
+
+//     for (i = 1; i < 19; i++) {
+//         sum = numOne + numSecond
+//         numOne = numSecond
+//         numSecond = sum
+//         console.log(sum)
+//     }
+// }
+
+// newFunction()
