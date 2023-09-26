ac17e3d7541d6d0c91145c8e6154aa762c09338c
Safarova1 <samasafarova1998@gmail.com>
2023-09-26 21:51:47 +0400

    lesson 5

--- JavaScript cw/lesson_5 Dövrlər dərs 2(teamsa bax olmamisan)/script.js
+++ JavaScript cw/lesson_5 Dövrlər dərs 2(teamsa bax olmamisan)/script.js
@@ -106,26 +106,60 @@
 
 // ----------------------------------------------------------------------------------------
 
-let i = 0
-const numPlus = () => {
-    i++
-    return i
-}
+// let i = 0
+// const numPlus = () => {
+//     i++
+//     return i
+// }
 
 
-console.log(numPlus)
-console.log(numPlus)
-
-
-
+// console.log(numPlus)
+// console.log(numPlus)
 
 
 
 
 
+// ----------------------------------------------------------------------------------------
+// close klouze funksiyasi qapanma funksiyasi
+//   ---lokal deyiseni isletmek ucun istifade olunur
+// return-den sonra hecne islemeyecek
+
+// 1
+// const numPlus = () => {
+
+//     let i = 0
+
+//     return () => {
+//         i++
+//         console.log(i)
+//     }
+// }
+
+// let newFunction = numPlus()
+// newFunction()
+// newFunction()
 
 
 
+
+// 2
+// const numPlus = () => {
+
+//     let i = 0
+
+//     return () => { //qapanma
+//         i++
+//         return i
+//     }
+// }
+
+// let newFunction = numPlus()
+// let num = newFunction()
+// num = newFunction()
+// num = newFunction()
+// num = newFunction()
+// console.log(num)
 
 
 
