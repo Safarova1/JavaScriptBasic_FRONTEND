f14438fc336002d5cef735263194e207884ec33e
Safarova1 <samasafarova1998@gmail.com>
2023-09-29 11:37:37 +0400

    l 8

--- JavaScript cw/lesson_8 massiv/script.js
+++ JavaScript cw/lesson_8 massiv/script.js
@@ -1,1 +1,82 @@
-//massiv
+// //massiv
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(array[0], array[1])
+
+// //metodlar
+
+// //axira eleva olunur
+// array.push(11)
+// console.log(array)
+
+// //axirindan silir
+// array.pop()
+// console.log(array)
+
+// //evvelinden silir
+// array.shift()
+// console.log(array)
+
+// //evvele elave olunur
+// array.unshift(9)
+// console.log(array)
+
+
+
+
+
+
+// //hemen indexsin deyerini qaytarir
+// console.log(array.at(1))
+
+
+// //massivde olan 3un indexsin
+// console.log(array.indexOf(1))
+
+
+// //elementin olub olmadigini yoxlayir
+// console.log(array.includes(2)) //true
+
+
+//----------------------------------------------------------------------------------------------
+// //concat birlewdirmek
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let arraySecond = [12, 13, 14, 15]
+
+// //concat birlewdirmek
+// let newArray = array.concat(arraySecond)
+
+// console.log(newArray)
+
+
+
+
+
+//----------------------------------------------------------------------------------------------
+//icindeki 1 skobkani acir --- flat
+
+let innerArray = [1, 2, [3, 4]]
+
+console.log(innerArray[2][0]) //3
+
+//[1, 2, 3, 4]
+console.log(innerArray.flat())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
