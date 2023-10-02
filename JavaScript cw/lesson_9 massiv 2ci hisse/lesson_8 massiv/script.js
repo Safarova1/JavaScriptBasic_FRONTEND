542d454616a8289ff0711f7ef0111bbcb5159285
Safarova1 <samasafarova1998@gmail.com>
2023-10-02 12:17:07 +0400

    lesson 9

--- JavaScript cw/lesson_9 massiv 2ci hisse/lesson_8 massiv/script.js
+++ JavaScript cw/lesson_9 massiv 2ci hisse/lesson_8 massiv/script.js
@@ -3,6 +3,37 @@
 
 
 
+//-------------------------------------------------------------------------------------------
+// splice --1ci indexsi yazirsan hardan silmeye baslayacaq sora 2ci nece element sileceyini gosteririk
+// slice --hansi indexden hansina kimi secilmelidir
+
+// let arr = [1,2,3,4,5,6,7]
+// arr.splice(2,3)   // 2ci indexden baslayib 3 element silir
+// console.log(arr)
+// let newArr = arr.slice(1,3)  //1ci indexden 3cu indexse kimi olan hisse
+// console.log(newArr)
+
+// console.log(arr.some((item) => item > 6))
+
+
+
+
+
+//-------------------------------------------------------------------------------------------
+// findIndex -- elementi verirsen indexsi tapir
+
+
+// let input = prompt()
+// let deletedElement
+
+// let arr = ['Ramin', 'Nahid','Samir']
+
+// deletedElement = arr.findIndex((item) => item === input)
+
+
+// arr.splice(deletedElement,1)
+
+// console.log(arr)
 
 
 
@@ -10,11 +41,112 @@
 
 
 
+//-------------------------------------------------------------------------------------------
+// filter -- hemen elementi silir item !==input  /// hemen elementi saxlayir item ===input
+
+
+// zadaca1
+// let input = prompt()
+
+// let arr = ['Ramin', 'Nahid','Samir']
+
+// let newArr = arr.filter((item) => item === input)
+
+// // if(arr.includes(input)){
+// //     arr = arr.filter((e) => e !== input)
+// // }
+
+// console.log(arr)
+// console.log(newArr)
+
+
+
+
+// zadaca2
+// let numArr =[1,2,3,4,5,6,7,8,9]
+
+// numArr = numArr.filter((item) => item < 6)
+
+// console.log(numArr)
 
 
 
 
 
+
+// zadaca3
+// let input = prompt()
+
+// let numarr = input.split(' ')
+// console.log(numarr)
+// let newnum = []
+// for (let index = 0; index < numarr.length; index++) {
+//     const element = Number(numarr[index])
+//     newnum.push(element)
+// }
+// newnum = newnum.filter((e) => e < 3)
+// console.log(newnum)
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------
+// map -- massivin butun elementlerini biz istediyimiz kimi deyiwir
+
+
+// zadaca1
+// let input = prompt()
+
+// let numarr = input.split(' ')
+// console.log(numarr)
+
+// numarr = numarr.map((item) => Number(item))
+// numarr = numarr.filter((element) => element < 3)
+// console.log(numarr)
+
+
+
+
+
+// zadaca2
+// let myArr = [2, 3, 4, 5, 6, 7]
+
+// myArr = myArr.map((item) => {
+//     if (item % 2 === 0) {
+//         return item * 2
+//     } else {
+//         return item * 4
+//     }
+// })
+
+
+// console.log(myArr)
+
+
+
+
+
+
+
+// zadaca3
+
+// let even = []
+// let odd = []
+
+// let myArr = [1,2,3,4,5,6]
+// myArr = myArr.map((item) => {
+//     if(item % 2 === 0){
+//         even.push(item)
+//     }else{
+//         odd.push(item) 
+//     }
+// })
+
+// console.log(myArr)
+// console.log(even)
+// console.log(odd)
 
 
 
