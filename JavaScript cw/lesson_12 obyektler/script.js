440801de9f37df246eb4ac10f49296500bc17280
Safarova1 <samasafarova1998@gmail.com>
2023-10-09 12:29:48 +0400

    l 12

---
+++ JavaScript cw/lesson_12 obyektler/script.js
@@ -0,0 +1,101 @@
+// //1ci usul
+// let obj = {
+//     studentName: 'Alex',
+//     profession: 'developer'
+// }
+
+// console.log(obj.studentName, obj.profession)
+
+
+
+// // 2ci usul
+// let newObj = new Object()
+// newObj.studentNam = 'Alex'
+// newObj.profession = 'Developer'
+
+
+// console.log(newObj['studentNam'])
+
+
+
+// console.log(obj, newObj)
+
+
+
+
+
+
+//-----------------------------------------------------------------------
+// // eger istiyirem deyisenle obyektin acarlarina uygun gonderim
+
+
+// let object = {
+//     'student Name': 'Alex',
+//     'profession':'developer'
+// }
+
+
+// let input = prompt()  // student Name ---> Alex  alacam   (profession ---> develoer) 
+
+
+// console.log(object[input])  
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------
+
+
+//massivin elementleri obyektlerdi
+let arr = [
+    {
+        'student Name': 'Alex',
+        'profession': 'developer',
+        id: 1
+
+    },
+    {
+        'student Name': 'Alex',
+        'profession': 'doctor',
+        id: 2
+
+    },
+    {
+        'student Name': 'Sam',
+        'profession': 'developer',
+        id: 3
+
+    },
+
+]
+
+// let input = prompt()
+// let newarr = arr.filter((item) => item[input] === 'developer')
+
+// console.log(newarr)
+
+
+
+let inputStudent =prompt()
+let inputProfession = prompt()
+
+let newarr = arr.filter((item)=> item[inputStudent]===inputProfession)
+
+console.log(newarr)   //student Name: 'Sam', profession: 'developer', id: 3
+
+//1ci usul 
+// console.log(newarr[0]['student Name'])
+
+
+//2ci usul
+// newarr.forEach((item) => {
+//     console.log(item['student Name'])
+// })
