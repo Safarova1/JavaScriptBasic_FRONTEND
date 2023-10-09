c27defbf3d999ed0b6bbf949b443335127f4a993
Safarova1 <samasafarova1998@gmail.com>
2023-10-09 13:50:39 +0400

    lesson 12

--- JavaScript cw/lesson_12 obyektler/praktika 3/script.js
+++ JavaScript cw/lesson_12 obyektler/praktika 3/script.js
@@ -2,6 +2,11 @@
 // funkciya her cagrilanda input ile daxil edilir massive obj elave edir  oftomatik id boyuyur
 // 
 
+
+
+// 3. AddObject adlı function yaradın. Bu funksiyanı hər dəfə çağıranda propmt çıxsın. Prompta ad daxil edin. Ad daxil olunan kimi bir boş massive avtomatik object yazılacaq. Objectin içində avtomatik böyüyən id yazılacaq və daxil olunan ad
+
+
 let massiv = []
 let count = 1
 
