c87154e99dc4e5d9bf0c816f794e7e972d5b373b
Safarova1 <samasafarova1998@gmail.com>
2023-10-11 12:18:26 +0400

    l 13

--- JavaScript cw/lesson_13 obyektler2ci hisse/script.js
+++ JavaScript cw/lesson_13 obyektler2ci hisse/script.js
@@ -70,28 +70,42 @@
 //---------------------------------------------------------------------------------------
 
 //1 obyektin icinde o biri byekt cagira biler
+//this -ancaq obyektlerde isleyir
 
 
-let object = {
-    userName: 'Ramin',
-    changeName(newName) {
-        this.userName = newName
-    },
-    printName() {
-        console.log(this)  //butun obyekti qaytarir
-        console.log(this.userSettings)  //usersettingin her seyini alir
-    },
-    userSettings: {
-        active: true,
-        printActive() {
-            console.log(this.active)
-        }
-    }
-}
-object.printName()
-object.changeName('Samir')
-object.printName()
-object.userSettings.printActive()
+// let object = {
+//     userName: 'Ramin',
+//     changeName(newName) {
+//         this.userName = newName
+//     },
+//     printName() {
+//         console.log(this)  //butun obyekti qaytarir
+//         console.log(this.userSettings)  //usersettingin her seyini alir
+//     },
+//     userSettings: {
+//         active: true,
+//         printActive() { //metod
+//             console.log(this.active)
+//         }
+//     }
+// }
+// object.printName()
+// object.changeName('Samir')
+// object.printName()
+// object.userSettings.printActive()
+
+
+
+
+
+
+
+
+
+
+
+// teze massiv yaratmaq
+// let array = new Array(1,2,3,4)
 
 
 
@@ -128,6 +142,62 @@ object.userSettings.printActive()
 // let obj = arr.find((item) => item.id === input)
 // console.log(obj)
 // alert(obj.userId + obj.title + obj.body)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------
+// this obyektlerde islenir 
+// elave Array FormData String ve s hazir obyektler var onlarda da islede bilerik
+
+// function f(ev){
+//     ev.preventDefault()
+//     let formData = new FormData(this)
+//     let arr = [...formData.entries()]
+//     console.log(arr)
+// }
+
+// form.addEventListener('submit', f)
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------
+//hazir obyeklerle is
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
