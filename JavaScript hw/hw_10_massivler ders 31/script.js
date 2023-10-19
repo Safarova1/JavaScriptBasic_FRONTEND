0b8e59ef184e5756e1c2286fc633a75778e4ada7
Safarova1 <samasafarova1998@gmail.com>
2023-10-19 20:13:25 +0400

    10

--- JavaScript hw/hw_10_massivler ders 31/script.js
+++ JavaScript hw/hw_10_massivler ders 31/script.js
@@ -1,9 +1,21 @@
-const student = [
-    ["Nigar", "Aliyeva", 85],
-    ["Orxan", "Memmedov", 92],
-    ["Ferid", "Husetyn", 75],
-    ["Zahida", "Quliyeva", 88],
-];
+const telebeler = [
+    ["Nigar","Əliyeva",85 ],
+    ["Orxan","Məmmədov",92 ],
+    ["Fərid","Hüseynov",75 ],
+    ["Zahidə","Quliyeva",82 ],
+  ];
 
+
+
+ let sortedTələbələr = telebeler.sort((a,b)=>a[2]- b[2])
+
+ console.log(sortedTələbələr)
+
+
+
+let newtelebeler = [...new Set(telebeler)]
+
+
+console.log(newtelebeler)
 
 
--- JavaScript hw/hw_10_massivler ders 32/script.js
+++ JavaScript hw/hw_10_massivler ders 32/script.js
@@ -1,21 +1,34 @@
-const telebeler = [
-    ["Nigar","Əliyeva",85 ],
-    ["Orxan","Məmmədov",92 ],
-    ["Fərid","Hüseynov",75 ],
-    ["Zahidə","Quliyeva",82 ],
-  ];
+const student = [
+    ["Nigar", "Aliyeva", 85],
+    ["Orxan", "Memmedov", 92],
+    ["Ferid", "Husetyn", 75],
+    ["Zahida", "Quliyeva", 85],
+];
 
 
+//siraladim
+let sortedStudent = student.sort((a, b) => a[2] - b[2])
 
- let sortedTələbələr = telebeler.sort((a,b)=>a[2]- b[2])
+//eyni qiymetlileri cixartdim
+let item2Student = []
+sortedStudent.forEach((item) => {
+    item2Student.push(item[2])
+})
+let newStudent = [... new Set(item2Student)]
 
- console.log(sortedTələbələr)
+//ortalamani gosterdim
+let ortalama = (newStudent.reduce((a, b) => a + b)) / newStudent.length
+console.log(ortalama)
+
+//ortama uzerileri gosterdim
+let ortalamaUzeri = sortedStudent.filter((item) => item[2] > ortalama)
+console.log(ortalamaUzeri)
 
 
-
-let newtelebeler = [...new Set(telebeler)]
-
-
-console.log(newtelebeler)
+//en yuksek en asagi alanlar
+let minBal = sortedStudent[0][0]
+console.log(minBal)
 
 
+let maxBal = sortedStudent.at(-1)[0]
+console.log(maxBal)
