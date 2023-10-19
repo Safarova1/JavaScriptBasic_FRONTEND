6dc31774504153c76bfb0982ee4ce5e081e6e808
Safarova1 <samasafarova1998@gmail.com>
2023-10-19 19:41:54 +0400

    duzenleme

---
+++ JavaScript hw/hw_10_massivler ders 31/script.js
@@ -0,0 +1,9 @@
+const student = [
+    ["Nigar", "Aliyeva", 85],
+    ["Orxan", "Memmedov", 92],
+    ["Ferid", "Husetyn", 75],
+    ["Zahida", "Quliyeva", 88],
+];
+
+
+
---
+++ JavaScript hw/hw_15_setTimeout ve setInterval 2/script.js
@@ -0,0 +1,159 @@
+//alinmadi
+
+// let input = prompt("Ramin Nasraddinzade-ramin98-password")
+
+// let massiv = []
+// let obyekt = {}
+// if (input !== "show") {
+
+
+//     // } else {
+
+//     let currentTime = new Date
+//     let gun = currentTime.getDate()
+//     let saat = currentTime.getHours()
+
+//     let arr = input.split("-")
+
+//     obyekt = {
+//         name: arr[0],
+//         login: arr[1],
+//         parol: arr[2],
+//         date: [
+//             {
+//                 day: gun,
+//                 clock: saat
+//             }
+//         ]
+//     }
+//     massiv.push(obyekt)
+
+//     console.log(obyekt)
+//     console.log(massiv)
+//     clearInterval(stopTime)
+
+
+// } else {
+//     console.log("poka")
+//     console.log(obyekt)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = []
+
+// let obb = new Object()
+
+// let qlavniyMassiv = [obb]
+
+// console.log(qlavniyMassiv)
+
+// let stopArray = setInterval(() => {
+
+//     let input = prompt("Ramin Nasraddinzade-ramin98-password")
+
+
+
+
+//     if (input == "show") {
+//         console.log(arr)
+//         console.log(Object.values(obb))
+
+//         clearInterval(stopArray)
+//     } else {
+
+//         arr = arr.push(input.split("-"))
+
+//         obb["name"] = arr[0]
+//         obb["login"] = arr[1]
+//         obb["password"] = arr[2]
+
+//     }
+
+// }, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+setInterval(() => {
+
+    let input = prompt("Ramin Nasraddinzade-ramin98-password")
+
+
+    if (input != "show") {
+        let input = prompt("Ramin Nasraddinzade-ramin98-password")
+
+
+        let arr = []
+        arr = input.split("-")
+
+        let massiv = []
+
+        massiv.push(arr)
+
+        let obb = new Object()
+
+        obb["name"] = arr[0]
+        obb["login"] = arr[1]
+        obb["password"] = arr[2]
+
+
+        let qlavniyMassiv = [obb]
+
+        console.log(qlavniyMassiv)
+    } else {
+        console.log(input)
+        clearInterval(stopArray)
+    }
+
+}, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
