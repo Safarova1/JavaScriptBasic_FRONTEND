95981e001c6fd4cbb562224b68d5e285fb5dbfdd
Safarova1 <samasafarova1998@gmail.com>
2023-10-23 13:42:11 +0400

    18

---
+++ JavaScript cw/lesson_18 classlar/praktika 1/script.js
@@ -0,0 +1,82 @@
+// Book classi yaradin. 
+// Bu classin icinde constructor yaradin. 
+// constructora uc xisusiyyet verin name, author,price.
+// Classin icinde ustelik method yaradin hansi ki kitabin
+// butun xususiyyetlerini gosterecek. 
+// Sonra bu klassdan obyekt yaradin,
+// obyekte deyerler verin ve methodu cagirib kitabin
+// obyekt xususiyyetlerini  gosterin
+
+
+
+
+// class Book {
+//     constructor(name, author, price) {
+//         this.name = name
+//         this.author = author
+//         this.price = price
+
+//     }
+
+//     vivodDannix() {
+//         console.log(`Kitabin adi-${this.name}, muellifi-${this.author}, deyeri-${this.price}`)
+//     }
+// }
+
+// let book1 = new Book('War and Peace', 'Lew Tolstoy', 35)
+
+
+// book1.vivodDannix()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//zadac2
+// name autor shopname shopauthor + masssiv evvelceden bos metod elave edir kitab
+// massive aoyekter yazirik hemen obyekti gosteirrik
+
+
+class Shop {
+    constructor(name, author ) {
+        this.shopName = name
+        this.shopAdress = author
+        this.book = []
+
+    }
+
+    pushBook() {
+        console.log(`Kitabin adi-${this.shopname}, muellifi-${this.shopauthor}, deyeri-${this.arr}`)
+    }
+
+    addBook() {
+while(true){
+    let input
+}
+
+    }
+}
+
+let inputname = prompt()
+let inputauthor = prompt()
+
+
+let book1 = new Shop(inputname, inputauthor, input)
+
+
+book1.vivodDannix()
+
+
+
--- JavaScript cw/lesson_18 classlar/script.js
+++ JavaScript cw/lesson_18 classlar/script.js
@@ -4,10 +4,97 @@ class Book {
         this.author = author
         this.price = price
     }
+    pushToArray(){
+        while(true){
+            let inputOne = prompt()
+            this.arr.push(inputOne)
+            if(inputOne === 'end'){
+                break
+            }
+        }
+    }
 
 }
 let book1 = new Book('voyna i mir', 'lev tolstoy', 35)
 let book2 = new Book('koroglu', 'milli', 45)
 let book3 = new Book('dede qorqud', 'milli', 55)
 
-console.log(book1, book2, book3)
+book1.pushToArray()
+console.log(book1)
+
+
+// let arr = [book1, book2, book3]
+// arr.forEach(item => {
+//     if (item.bookName == 'koroglu')
+//         console.log(item.bookName, item.author, item.price)
+// })
+
+
+
+
+
+
+// console.log(book1, book2, book3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Book {
+//     constructor(bookName, author, price, arr) {
+//         this.bookName = bookName
+//         this.author = author
+//         this.price = price
+
+//     }
+
+//     // pushToArray() {
+//     //     while (true) {
+//     //         let name = prompt()
+//     //         let author = prompt()
+//     //         let price = prompt('price')
+
+//     //         this.arr.push(new Book(name, author, price))
+//     //         if (price === 'end') {
+//     //             break
+//     //         }
+//     //     }
+//     // }
+//     showArray() {
+//         console.log(this.arr)
+//     }
+// }
+
+
+// // let inputOne = prompt()
+// let book1 = new Book('')
+// book1.pushToArray()
+// book1.showArray()
+
+// console.log(book1)
+
+// // let inputTwo = prompt()
+
+// // book2.pushToArray({name:inputTwo})
+// // console.log(book2)
+
+// // let arr = [book1,book2,book3]
+// // console.log(arr[1])
+// // arr.forEach(item => {
+// //     if(item.bookName === 'Koroglu'){
+// //         console.log(item.bookName,item.author,item.price)
+// //     }
+// // })
+// // console.log(book1,book2, book3)
