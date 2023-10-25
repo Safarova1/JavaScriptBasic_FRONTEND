d0455fa8254a2317b36f494ef5a5c3ad93a6c1f3
Safarova1 <samasafarova1998@gmail.com>
2023-10-25 19:33:14 +0400

    15

--- JavaScript hw/hw_15_setTimeout ve setInterval 2/script.js
+++ JavaScript hw/hw_15_setTimeout ve setInterval 2/script.js
@@ -139,40 +139,38 @@
 
 
 
-
-
-// setInterval(() => {
-
-let input = prompt("Ramin Nasraddinzade-ramin98-password")
-
-let arr = []
-arr= arr.push(input.split('-'))
-
-console.log(arr)
-
-let currentTime = new Date
-let deqiqe = currentTime.getMinutes()
-let saat = currentTime.getHours()
-
-let tarix = `saat: ${saat}:${deqiqe}`
-
-
-let obyekt = {
-    ad: arr[0],
-    login: arr[1],
-    parol: arr[2],
-    tarix: tarix
-}
-
-
 let massiv = []
-massiv.push(obyekt)
 
+let stopArray = setInterval(() => {
 
-console.log(massiv)
+    let input = prompt("ad,login,parol daxil edin,numune: Ramin Nasraddinzade-ramin98-password")
 
-//     clearInterval(setInterval)
-// }, 1000)
+    let arr = []
+    arr = input.split('-')
+
+    let currentTime = new Date
+    let deqiqe = currentTime.getMinutes()
+    let saat = currentTime.getHours()
+    let tarix = `saat: ${saat}:${deqiqe}`
+
+    let obyekt = {
+        ad: arr[0],
+        login: arr[1],
+        parol: arr[2],
+        tarix: tarix
+    }
+
+    if (input === 'show') {
+        massiv.forEach((item)=>alert(Object.values(item)) )
+    } else if (input === 'end') {
+        clearInterval(stopArray)
+
+    } else {
+        massiv.push(obyekt)
+        console.log(massiv)
+    }
+
+}, 1000)
 
 
 
