b3420be8a815540b0cb1ddcbf66e8d5d9156d42e
Safarova1 <samasafarova1998@gmail.com>
2023-10-25 20:06:22 +0400

    15

---
+++ JavaScript hw/hw_17 objects ders 5/script.js
@@ -0,0 +1,96 @@
+let favoriteGoods = []
+
+let shopGoods = [
+    {
+        shopName: 'Best Shop',
+        shopContacts: '312321',
+        shopAdress: 'Baki',
+
+        fruits: [
+            {
+                "qiymet": 45,
+                "tesvir": 'qirmizi',
+                "name": "Sumka",
+            },
+            {
+                "qiymet": 55,
+                "tesvir": 'qirmizi',
+                "name": "Eyaqqabi",
+            },
+            {
+                "qiymet": 40,
+                "tesvir": 'qirmizi',
+                "name": "cins",
+
+            },
+            {
+                "qiymet": 50,
+                "tesvir": 'qirmizi',
+                "name": "shalvar",
+            },
+            {
+                "qiymet": 18,
+                "tesvir": 'qirmizi',
+                "name": "papaq",
+            },
+        ]
+    },
+    {
+        shopName: 'My Shop',
+        shopContacts: '312321',
+        shopAdress: 'Baki',
+
+        fruits: [
+            {
+                "qiymet": 45,
+                "tesvir": 'qirmizi',
+                "name": "canta",
+            },
+            {
+                "qiymet": 55,
+                "tesvir": 'qirmizi',
+                "name": "don",
+            },
+            {
+                "qiymet": 40,
+                "tesvir": 'qirmizi',
+                "name": "krossovka",
+
+            },
+            {
+                "qiymet": 50,
+                "tesvir": 'qirmizi',
+                "name": "kofta",
+            },
+            {
+                "qiymet": 18,
+                "tesvir": 'qirmizi',
+                "name": "papaq",
+            },
+        ]
+    }
+]
+
+
+
+let input = prompt("istediyin shopName yaz - dayandirmaq ucun end yaz")
+
+
+shopGoods.forEach((item) => {
+    if (item.shopName === input) {
+        let inputname = prompt("istediyin mali yaz")
+        item.fruits.forEach((mal) => {
+            if (mal.name == inputname) {
+                favoriteGoods.push(mal)
+            }
+        })
+    }
+
+})
+
+
+
+
+console.log(favoriteGoods)
+
+
