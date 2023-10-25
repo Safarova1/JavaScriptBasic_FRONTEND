50e7aef0324d1680bfec7734b2eda5bcab00c074
Safarova1 <samasafarova1998@gmail.com>
2023-10-25 18:43:44 +0400

    15

--- JavaScript hw/hw_15_setTimeout ve setInterval 2/script.js
+++ JavaScript hw/hw_15_setTimeout ve setInterval 2/script.js
@@ -102,40 +102,38 @@
 
 
 
-setInterval(() => {
+// setInterval(() => {
 
-    let input = prompt("Ramin Nasraddinzade-ramin98-password")
-
-
-    if (input != "show") {
-        let input = prompt("Ramin Nasraddinzade-ramin98-password")
+//     let input = prompt("Ramin Nasraddinzade-ramin98-password")
 
 
-        let arr = []
-        arr = input.split("-")
-
-        let massiv = []
-
-        massiv.push(arr)
-
-        let obb = new Object()
-
-        obb["name"] = arr[0]
-        obb["login"] = arr[1]
-        obb["password"] = arr[2]
+//     if (input != "show") {
+//         let input = prompt("Ramin Nasraddinzade-ramin98-password")
 
 
-        let qlavniyMassiv = [obb]
+//         let arr = []
+//         arr = input.split("-")
 
-        console.log(qlavniyMassiv)
-    } else {
-        console.log(input)
-        clearInterval(stopArray)
-    }
+//         let massiv = []
 
-}, 1000)
+//         massiv.push(arr)
+
+//         let obb = new Object()
+
+//         obb["name"] = arr[0]
+//         obb["login"] = arr[1]
+//         obb["password"] = arr[2]
 
 
+//         let qlavniyMassiv = [obb]
+
+//         console.log(qlavniyMassiv)
+//     } else {
+//         console.log(input)
+//         clearInterval(stopArray)
+//     }
+
+// }, 1000)
 
 
 
@@ -143,12 +141,38 @@ setInterval(() => {
 
 
 
+// setInterval(() => {
+
+let input = prompt("Ramin Nasraddinzade-ramin98-password")
+
+let arr = []
+arr= arr.push(input.split('-'))
+
+console.log(arr)
+
+let currentTime = new Date
+let deqiqe = currentTime.getMinutes()
+let saat = currentTime.getHours()
+
+let tarix = `saat: ${saat}:${deqiqe}`
 
 
+let obyekt = {
+    ad: arr[0],
+    login: arr[1],
+    parol: arr[2],
+    tarix: tarix
+}
 
 
+let massiv = []
+massiv.push(obyekt)
 
 
+console.log(massiv)
+
+//     clearInterval(setInterval)
+// }, 1000)
 
 
 
