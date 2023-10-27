b55509364e830d32ff0fabb0959471c23ca984e9
Safarova1 <samasafarova1998@gmail.com>
2023-10-27 13:01:08 +0400

    20

--- JavaScript cw/lesson_20 abstrakt sinifler/script.js
+++ JavaScript cw/lesson_20 abstrakt sinifler/script.js
@@ -1,4 +1,4 @@
-//abstrakt class
+//abstrakt class ---serhedler qoymaq ucun koduma istifade ede bilerem
 
 // class Animal {
 //     constructor(name) {
@@ -37,37 +37,104 @@
 
 
 
-class Telebeler {
-    constructor(adi,soyadi){
-        this.adi=adi
-        this.soyadi=soyadi
-        if (new.target === Telebeler) {
-            throw new Error('siyahiya baxmaq olmaz')
-        }
-    }
-    sinifA() {
-        throw new Error('baglanib')
+// class Telebeler {
+//     constructor(adi,soyadi){
+//         this.adi=adi
+//         this.soyadi=soyadi
+//         if (new.target === Telebeler) {
+//             throw new Error('siyahiya baxmaq olmaz')
+//         }
+//     }
+//     sinifA() {
+//         throw new Error('baglanib')
 
-    }
-}
+//     }
+// }
 
-class Qizlar extends Telebeler {
-    constructor(adi,soyadi){
-        super(adi,soyadi)
-    }
-    balaca(){
-        console.log(qiz)
-        super.sinifA()
-    }
-}
-
-
-
-//metodu icinde sehvlik yatarmaq
-const qiz = new Qizlar("aydan",'cafarova')
-qiz.balaca()
+// class Qizlar extends Telebeler {
+//     constructor(adi,soyadi){
+//         super(adi,soyadi)
+//     }
+//     balaca(){
+//         console.log(qiz)
+//         super.sinifA()
+//     }
+// }
 
 
-// umumi abstrakt klassin sehvi
-const telebe = new Telebeler()
-console.log(telebe)
+
+// //metodu icinde sehvlik yatarmaq
+// const qiz = new Qizlar("aydan",'cafarova')
+// qiz.balaca()
+
+
+// // umumi abstrakt klassin sehvi
+// const telebe = new Telebeler()
+// console.log(telebe)
+
+
+
+
+
+
+
+
+
+
+
+
+//get set--obyekt acari kimi istifade edirik
+
+
+// numune
+
+// class Person {
+//     constructor(name, lastName) {
+//         this._name = name
+//         this._lastName = lastName
+//     }
+//     get personName() {
+//         return this._name + " " + this._lastName
+//     }
+
+//     set personName(fullname) {
+//         let arr = fullname.split(' ')
+//         this._name = arr[0]
+//         this._lastName = arr[1]
+//     }
+// }
+
+// const person = new Person('Ramin Nasraddinzade')
+// console.log(person._name)
+
+
+// person.personName = 'Samir Cavadov'
+// console.log(person._name,person._lastName)
+
+
+// console.log(person._name)
+// console.log(person._lastName)
+// console.log(person.personName)
+
+// console.log(person)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// praktik2
+
+
+
+
+
