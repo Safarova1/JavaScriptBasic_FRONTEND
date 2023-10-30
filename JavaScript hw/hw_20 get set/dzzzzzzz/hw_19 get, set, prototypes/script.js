78f5be785ce35525320130a3838fddce20b1465e
Safarova1 <samasafarova1998@gmail.com>
2023-10-30 11:29:01 +0400

    20

---
+++ JavaScript hw/hw_20 get set/dzzzzzzz/hw_19 get, set, prototypes/script.js
@@ -0,0 +1,240 @@
+let shopGoods = [
+    {
+        store_name: "Moda Dünyası",
+        store_address: "Bakı şəhəri, Nizami kuçəsi 5",
+        products: [
+            { "product_name": "Kişi köynəyi", "product_description": "Mavi rəngdə, 100% pamuk", "product_price": 50 },
+            { "product_name": "Qadın bluzası", "product_description": "Dəri detallı, qara rəngdə", "product_price": 60 },
+            { "product_name": "Kişi pantolonu", "product_description": "Qəhvəyi rəng, kənar cebi", "product_price": 75 },
+            { "product_name": "Qadın eteyi", "product_description": "Qırmızı rəng, mini", "product_price": 40 },
+            { "product_name": "Kişi dəsmalı", "product_description": "Nəqşdar dizayn", "product_price": 20 },
+            { "product_name": "Qadın çantası", "product_description": "Əlgötürən, dəri", "product_price": 90 },
+            { "product_name": "Kişi botları", "product_description": "Qış üçün, suya davamlı", "product_price": 120 },
+            { "product_name": "Qadın ayaqqabıları", "product_description": "Yüksək tapan, lacivərt", "product_price": 85 },
+            { "product_name": "Kişi papağı", "product_description": "Qara rəngdə, dəri", "product_price": 45 },
+            { "product_name": "Qadın şalvarı", "product_description": "Göy rəngdə, kaşmir", "product_price": 70 }
+        ]
+    },
+    {
+        store_name: "Zərif Moda",
+        store_address: "Bakı şəhəri, 28 May kuçəsi 12",
+        products: [
+            { "product_name": "Kişi palto", "product_description": "Süət dəri, qara rəngdə", "product_price": 250 },
+            { "product_name": "Qadın jaketi", "product_description": "Uzun, qaşqır", "product_price": 200 },
+            { "product_name": "Kişi kəməri", "product_description": "Dəri, metal tokalı", "product_price": 40 },
+            { "product_name": "Qadın badlonu", "product_description": "Retro stil, qızıl rəngdə", "product_price": 180 },
+            { "product_name": "Kişi şortu", "product_description": "Spor stil, elastik", "product_price": 60 },
+            { "product_name": "Qadın maykası", "product_description": "Boyalı nəqş, pamuklu", "product_price": 45 },
+            { "product_name": "Kişi saatı", "product_description": "Qara rəngdə, analog", "product_price": 150 },
+            { "product_name": "Qadın bəzək", "product_description": "Qızıl, komplekt", "product_price": 220 },
+            { "product_name": "Kişi atkısı", "product_description": "Xəzəl rəng, uzun", "product_price": 35 },
+            { "product_name": "Qadın bantı", "product_description": "Metal detallı, elastik", "product_price": 50 }
+        ]
+    },
+    {
+        store_name: "Əliyev Moda Mərkəzi",
+        store_address: "Bakı şəhəri, Azadlıq prospekti 89",
+        products: [
+            { "product_name": "Kişi kostyumu", "product_description": "İki parçalı, qara rəngdə", "product_price": 320 },
+            { "product_name": "Qadın kombinezonu", "product_description": "Yaz üçün, açıq rəng", "product_price": 150 },
+            { "product_name": "Kişi sviteri", "product_description": "Düz rəng, yüngül", "product_price": 70 },
+            { "product_name": "Qadın ziyafət geyimi", "product_description": "Düzənşən, yaz üçün", "product_price": 95 },
+            { "product_name": "Kişi sport formaları", "product_description": "Spandex, idman üçün", "product_price": 55 },
+            { "product_name": "Qadın bikini", "product_description": "Tropik nəqş, elastik", "product_price": 45 },
+            { "product_name": "Kişi çantası", "product_description": "Dizayner, əsas bölməsi", "product_price": 100 },
+            { "product_name": "Qadın ətri", "product_description": "Gül ətri, uzunömürlü", "product_price": 80 },
+            { "product_name": "Kişi şalı", "product_description": "Səliqəli, çoxrəngli", "product_price": 25 },
+            { "product_name": "Qadın sutyeni", "product_description": "Destəklənmiş, rahat", "product_price": 65 }
+        ]
+    }
+]
+
+
+
+
+
+
+
+
+class ChooseFind {
+    constructor(store_name, product_name) {
+        this.store_name = store_name
+        this.product_name = product_name
+    }
+
+    get product() {
+        shopGoods.forEach((item) => {
+            if (item.store_name === this.store_name) {
+                item.products.forEach((item) => {
+                    if (item.product_name === this.product_name) {
+                        console.log(item.product_name + ' ' + item.product_description + ' ' + item.product_price)
+                    }
+                })
+            }
+        })
+
+    }
+    set product(massiv) {
+        item.product_name = massiv[0]
+        item.product_description = massiv[1]
+        item.product_price = massiv[2]
+            
+        console.log(shopGoods)
+
+        
+
+    }
+}
+
+
+
+
+
+
+
+let inputstore_name = prompt("istediyin store_name qeyd et - dayandirmaq ucun end yaz")
+if (inputstore_name === 'end') {
+    alert("hecne elave olunmadi")
+} else {
+    let inputproduct_name = prompt("elave etmek istediyiniz product_name qeyd edin")
+    let gostermek = new ChooseFind(inputstore_name, inputproduct_name)
+    gostermek.product
+
+    let cavab = prompt("deyismek istiyirsen? yes no ")
+    if (cavab = 'yes') {
+        let  = prompt("deyismek istediyiniz product_name qeyd edin")
+        let inputproduct_description = prompt("deyismek istediyiniz product_description qeyd edin")
+        let inputproduct_price = Number(prompt("deyismek istediyiniz product_price qeyd edin"))
+
+
+        let arr = []
+        arr.push(inputproduct_name+inputproduct_description+inputproduct_price)
+
+        let gostermek = new ChooseFind(inputstore_name, inputproduct_name)
+        gostermek.product=arr
+
+
+        alert('melumat deyisildi')
+    } else {
+        alert("melumat deyisilmedi")
+    }
+}
+
+// let arr = new See("")
+
+// // arr.productSee = shopGoods
+// arr.productSee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class ChangeFind {
+ 
+
+//     get product() {
+//         shopGoods.forEach((item) => {
+//             console.log(item.store_name)
+//             item.products.forEach((item) => {
+//                 console.log(item.product_name+' '+item.product_description+' '+item.product_price)
+//             })
+
+//         })
+//     }
+//     set product(shop) {
+
+//         shop.forEach((item)=>{
+//             item.products.sort((a, b) => a.product_price - b.product_price)
+//         })
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class AddProduct {
+//     constructor(store_name, product_name, product_description,product_price) {
+//         this.store_name = store_name
+//         this.product_name = product_name
+//         this.product_description = product_description
+//         this.product_price = product_price
+//         this.mal = {}
+//     }
+
+//     addProduct(massiv) {
+//         massiv.forEach((item) => {
+//             if (item.store_name === this.store_name) {
+//                 this.mal['product_name'] = this.product_name
+//                 this.mal['product_description'] = this.product_description
+//                 this.mal['product_price'] = this.product_price
+//                 item.products.push(this.mal)
+//                 console.log(massiv)
+//             }
+//         })
+//     }
+// }
+
+
+
+// let inputstore_name = prompt("istediyin store_name qeyd et - dayandirmaq ucun end yaz")
+// if (inputstore_name === 'end') {
+//     alert("hecne elave olunmadi")
+// } else {
+//     let inputproduct_name = prompt("elave etmek istediyiniz product_name qeyd edin")
+//     let inputproduct_description = prompt("elave etmek istediyiniz product_description qeyd edin")
+//     let inputproduct_price = Number(prompt("elave etmek istediyiniz product_price qeyd edin"))
+
+//     let add = new AddProduct(inputstore_name, inputproduct_name, inputproduct_description, inputproduct_price)
+//     add.addProduct(shopGoods)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
